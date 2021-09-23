5c4960c960880f2a21888f91a31ef51f1a828c0f
sukjong2002 <sukjong2002@gmail.com>
2021-09-24 00:35:20 +0900

    init

--- background.js
+++ background.js
@@ -1,12 +1,13 @@
 async function replaceUSDWithETH() {
     // load price from coingecko
     const data = await fetch(
-        "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd"
+        "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=krw"
     ).then((response) => response.json());
-    const etherPrice = data.ethereum.usd;
+    const etherPrice = data.ethereum.krw;
 
     // replace simple dollar amounts
-    const simpleDollarRegex = /\${1}(\d+(,\d{3})*(\.[0-9]*)?)/g;
+    // const simpleDollarRegex = /\${1}(\d+(,\d{3})*(\.[0-9]*)?)/g;
+    const simpleDollarRegex = /(\d+(,\d{3})*(\.[0-9]*)?)원{1}/g
     document.body.innerHTML = document.body.innerHTML.replace(
         simpleDollarRegex,
         (match, rawDollarAmount) => {
@@ -53,14 +54,70 @@ async function replaceUSDWithETH() {
     );
 
     // replace cases where the dollar sign is in a separate span
-    const separateSpanRegex =
-        /<span (.*?)>\$<\/span>\s*<span (.*?)>(\d+(,\d{3})*(\.[0-9]*)?)<\/span>/g;
+    // const separateSpanRegex =
+    //     /<span (.*?)>\$<\/span>\s*<span (.*?)>(\d+(,\d{3})*(\.[0-9]*)?)<\/span>/g;
+    const separateSpanRegex = /<span (.*?)>(\d+(,\d{3})*(\.[0-9]*)?)<\/span>\s*<span (.*?)>\원<\/span>/g
     document.body.innerHTML = document.body.innerHTML.replace(
         separateSpanRegex,
-        (match, attrs1, attrs2, rawDollarAmount) => {
+        (match, attrs1, rawDollarAmount, attrs2, attrs3, attrs5 ) => {
             const dollarAmount = +rawDollarAmount.replace(/\,/g, "");
             const ethAmount = dollarAmount / etherPrice;
-            return `<span ${attrs1}>ETH</span><span ${attrs2}>${ethAmount.toLocaleString(
+            return `<span ${attrs5}>ETH</span><span ${attrs1}>${ethAmount.toLocaleString(
+                "en-US",
+                {
+                    maximumFractionDigits: 4,
+                }
+            )}</span>`;
+        }
+    );
+
+    //replace money in Coupang search list.
+    const cpngListSpanRegex = /<strong (.*?)>(\d+(,\d{3})*(\.[0-9]*)?)<\/strong>\s*\원/g
+    document.body.innerHTML = document.body.innerHTML.replace(
+        cpngListSpanRegex,
+        (match, attrs1, rawDollarAmount ) => {
+            const dollarAmount = +rawDollarAmount.replace(/\,/g, "");
+            const ethAmount = dollarAmount / etherPrice;
+            return `<span>ETH</span><span ${attrs1}>${ethAmount.toLocaleString(
+                "en-US",
+                {
+                    maximumFractionDigits: 4,
+                }
+            )}</span>`;
+        }
+    );
+
+    //replace money in Coupang product page.
+    const cpngPrdSpanRegex = /<strong>(\d+(,\d{3})*(\.[0-9]*)?)\s*<span (.*?)>\원<\/span><\/strong>/g
+    document.body.innerHTML = document.body.innerHTML.replace(
+        cpngPrdSpanRegex,
+        (match, rawDollarAmount, attrs1 ) => {
+            const dollarAmount = +rawDollarAmount.replace(/\,/g, "");
+            const ethAmount = dollarAmount / etherPrice;
+            return `<span>ETH</span><span ${attrs1}>${ethAmount.toLocaleString(
+                "en-US",
+                {
+                    maximumFractionDigits: 4,
+                }
+            )}</span>`;
+        }
+    );
+
+    //replace money in Naver auto page.
+    const naverAutoSpanRegex = /<span (.*?)>(\d+(,\d{3})*(\.[0-9]*)?)\~(\d+(,\d{3})*(\.[0-9]*)?)<\/span>\s*<span (.*?)>\만원<\/span>/g
+    document.body.innerHTML = document.body.innerHTML.replace(
+        naverAutoSpanRegex,
+        (match, attrs1, rawDollarAmountFirst, attrs2, attrs3, rawDollarAmountTwo, attrs5, attrs6, attrs8 ) => {
+            const dollarAmountFirst = +rawDollarAmountFirst.replace(/\,/g, "");
+            const ethAmountFirst = (dollarAmountFirst*10000) / etherPrice;
+            const dollarAmountTwo = +rawDollarAmountTwo.replace(/\,/g, "");
+            const ethAmountTwo = (dollarAmountTwo*10000) / etherPrice;
+            return `<span ${attrs8}>ETH</span><span ${attrs1}>${ethAmountFirst.toLocaleString(
+                "en-US",
+                {
+                    maximumFractionDigits: 4,
+                }
+            )}~${ethAmountTwo.toLocaleString(
                 "en-US",
                 {
                     maximumFractionDigits: 4,
